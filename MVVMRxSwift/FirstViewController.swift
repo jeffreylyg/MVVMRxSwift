ed9e03f80c7a958897626b2be5819c456c2c3cce
Yuguo Li <jeffreylyg@gmail.com>
2017-01-05 11:08:43 +0800

    Initial Commit

---
+++ MVVMRxSwift/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  MVVMRxSwift
+//
+//  Created by Yuguo Li on 2017/1/5.
+//  Copyright © 2017年 Yuguo Li. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ MVVMRxSwift/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  MVVMRxSwift
+//
+//  Created by Yuguo Li on 2017/1/5.
+//  Copyright © 2017年 Yuguo Li. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
